180cc99de6a1e40bc5da44a8023de7be797479b7
surrus <xsurrus@gmail.com>
2015-09-18 07:35:24 -0600

    arrowkey event handling

--- xnake.py
+++ xnake.py
@@ -2,17 +2,40 @@ import pygame
 
 pygame.init()
 
+white = (255,255,255)
+black = (0,0,0)
+red = (255,0,0)
+green = (0,255,0)
+
 gameDisplay = pygame.display.set_mode((800,600))
 pygame.display.set_caption('xnake')
 
-pygame.display.update()
+lead_x = 300
+lead_y = 300
 
 run = True 
 while run:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       run = False;
-    #print(event)
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_UP:
+        lead_y -= 10
+      if event.key == pygame.K_DOWN:
+        lead_y += 10
+      if event.key == pygame.K_LEFT:
+        lead_x -= 10
+      if event.key == pygame.K_RIGHT:
+        lead_x += 10
+
+
+
+  gameDisplay.fill(white)
+  pygame.draw.rect(gameDisplay, black, [lead_x,lead_y,10,10])
+
+  gameDisplay.fill(red, rect=[200,200,50,50])
+
+  pygame.display.update()
 
 pygame.quit()
 quit()
